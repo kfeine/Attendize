f838a54f8a43bb19e238b8df6aa7ac33157758b8
Benjamin N <ben.narang@laposte.net>
2017-10-10 23:02:56 +0200

    Quelques traductions en Français

---
+++ resources/lang/fr/mailers_ticketmailer_sendattendeeinvite.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'hi' => 'Bonjour, :firstname,',
+    'invited' => 'Vous avez été invité pour :title.',
+    'ticket' => 'Votre ticket pour :title est disponible en pièce-jointe.',
+    'regards' => 'Cordialement,'
+
+];
