7b58069d0eb2e10dec6acf8779e6f83daa8b211f
Benjamin N <ben.narang@posteo.net>
2017-11-28 20:49:17 +0100

    Allow formulas without options

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -138,11 +138,11 @@ class EventCheckoutController extends Controller
             //validation options
             $option_ids = $request->get('attendee_' .$attendee_id. '_options_'.$ticket_id);
             $options = [];
-            foreach($option_ids as $option_id){
+            foreach((array)$option_ids as $option_id){
                 if (!(bool)$request->get('attendee_' .$attendee_id. '_option_'. $ticket_id .'_'. $option_id)) {
                     continue;
                 }
-              
+
                 $option = TicketOptions::find($option_id);
                 if($option->ticket_id != $ticket_id || !$option->is_enabled){
                     continue; 
