98b8b4a9065fbe7d236697f48b22db7f5498aeae
Benjamin N <ben.narang@posteo.net>
2017-12-20 23:53:03 +0100

    Questions attendee id fix

--- resources/views/Public/ViewEvent/Partials/EventCreateOrderSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventCreateOrderSection.blade.php
@@ -143,7 +143,7 @@
                                                     {!! Form::text("ticket_holder_email[{$ticket['attendee_id']}][{$ticket['ticket']['id']}]", $ticket['attendee']['email'], ['required' => 'required', 'class' => "required ticket_holder_email.{$ticket['attendee_id']}.{$ticket['ticket']['id']} ticket_holder_email form-control"]) !!}
                                                 </div>
                                             </div>
-                                            @include('Public.ViewEvent.Partials.AttendeeQuestions', ['ticket' => $ticket['ticket'],'attendee_number' => $total_attendee_increment++])
+                                            @include('Public.ViewEvent.Partials.AttendeeQuestions', ['ticket' => $ticket['ticket'],'attendee_id' => $ticket['attendee_id'],'attendee_number' => $total_attendee_increment++])
 
                                         </div>
 
