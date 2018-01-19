5fa22c2af6c75020b03697a026cf3073f441e57b
Benjamin N <ben.narang@posteo.net>
2018-01-19 13:27:55 +0100

    Blank order page

--- resources/views/ManageEvent/Partials/AttendeesBlankSlate.blade.php
+++ resources/views/ManageEvent/Partials/AttendeesBlankSlate.blade.php
@@ -14,8 +14,5 @@
 @stop
 
 @section('blankslate-body')
-<button data-invoke="modal" data-modal-id='InviteAttendee' data-href="{{route('showInviteAttendee', array('event_id'=>$event->id))}}" href='javascript:void(0);'  class=' btn btn-success mt5 btn-lg' type="button" >
-    <i class="ico-user-plus"></i>
-    @lang('manageevent_partials_attendeesblankslate.invite_attendee')
-</button>
+
 @stop
