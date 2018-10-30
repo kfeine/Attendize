30081c032bfe7eeb6c5476a5d2f8981912020d9e
Benjamin N <ben.narang@laposte.net>
2018-10-30 15:42:40 +0100

    Translations

--- resources/lang/en/controllers_eventcheckoutcontroller.php
+++ resources/lang/en/controllers_eventcheckoutcontroller.php
@@ -2,19 +2,20 @@
 
 return [
 
-    'payment_cancelled' => 'You cancelled your payment. You may try again.',
+    'payment_cancelled'        => 'You cancelled your payment. You may try again.',
     'problem_processing_order' => 'Whoops! There was a problem processing your order. Please try again.',
-    'no_tickets_selected' => 'No tickets selected',
-    'max_number_tickets' => 'The maximum number of tickets you can register is :quantity',
-    'number_tickets' => 'You must select at least :min_per_person tickets.',
-    'first_name' => 'Ticket holder :person\'s first name is required',
-    'last_name' => 'Ticket holder :person\'s last name is required',
-    'email_required' => 'Ticket holder :person\'s email is required',
-    'email_invalid' => 'Ticket holder :person\'s email appears to be invalid',
-    'question_required' => 'This question is required',
-    'enable_javascript' => 'Please enable Javascript in your brower',
-    'session_expired' => 'Your session has expired',
-    'no_gateway_configured' => 'No payment gateway configured',
-    'try_again' => 'Sorry, there was an error processing your payment. Please try again.',
+    'no_tickets_selected'      => 'No tickets selected',
+    'max_number_tickets'       => 'The maximum number of tickets you can register is :quantity',
+    'number_tickets'           => 'You must select at least :min_per_person tickets.',
+    'first_name'               => 'Ticket holder :person\'s first name is required',
+    'last_name'                => 'Ticket holder :person\'s last name is required',
+    'email_required'           => 'Ticket holder :person\'s email is required',
+    'email_invalid'            => 'Ticket holder :person\'s email appears to be invalid',
+    'question_required'        => 'This question is required',
+    "privacy_policy_required"  => "Please accept the privacy policy",
+    'enable_javascript'        => 'Please enable Javascript in your brower',
+    'session_expired'          => 'Your session has expired',
+    'no_gateway_configured'    => 'No payment gateway configured',
+    'try_again'                => 'Sorry, there was an error processing your payment. Please try again.',
 
 ];
