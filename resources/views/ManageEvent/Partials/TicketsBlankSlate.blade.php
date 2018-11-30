c308e00c7e7106c3d3b628e7ce398c3b05d3071d
Pierre Galvez <login@pierre-galvez.fr>
2018-11-30 14:13:52 +0100

    fix errror on button

--- resources/views/ManageEvent/Partials/TicketsBlankSlate.blade.php
+++ resources/views/ManageEvent/Partials/TicketsBlankSlate.blade.php
@@ -13,7 +13,7 @@
 @stop
 
 @section('blankslate-body')
-    <button data-invoke="modal" data-modal-id='CreateTicket' data-href="{{route('showCreateTicket', array('event_id'=>$event->id))}}" href='javascript:void(0);'  class=' btn btn-success mt5 btn-lg' type="button" >
+    <button data-modal-id='CreateTicket' data-href="{{route('showCreateTicket', array('event_id'=>$event->id))}}"  class='loadModal btn btn-success mt5 btn-lg' type="button" >
         <i class="ico-ticket"></i>
         @lang('manageevent_partials_ticketsblankslate.create')
     </button>
