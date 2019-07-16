47bb5cdbd19162d42a771405540eec416377d97f
Benjamin N <ben.narang@posteo.net>
2019-07-16 15:37:07 +0200

    XSS vulnerability (see https://github.com/Attendize/Attendize/releases/tag/v1.1.2)

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -14,9 +14,10 @@ class Ticket extends MyBaseModel
      * @var array $rules
      */
     public $rules = [
-        'title'              => ['required'],
+        'title'              => ['required', 'alpha_dash'],
         'price'              => ['required', 'numeric', 'min:0'],
         'start_sale_date'    => ['date'],
+        'description'        => ['alpha_dash'],
         'end_sale_date'      => ['date', 'after:start_sale_date'],
         'quantity_available' => ['integer', 'min:0'],
     ];
